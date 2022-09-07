71b34b2072f9257c054be884f03f6853fe01919a
TheMacrom <flores.gaston2@gmail.com>
2022-09-07 14:20:19 -0300

    quite comillas a true y false

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -89,9 +89,9 @@ function estaEnRango(numero) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (numero<50 && numero>20){
-    return "true";
+    return true;
   }else{
-    return "false";
+    return false;
   }
 }
 
@@ -104,9 +104,9 @@ function esEntero(numero) {
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
   if (numero%1===0){
-    return "true";
+    return true;
   }else{
-    return "false";
+    return false;
   }
 }
 
