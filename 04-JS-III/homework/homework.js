9f1138e45fa6107a10bf5753b9e4ed15e6897721
TheMacrom <flores.gaston2@gmail.com>
2022-09-14 12:22:36 -0300

    ejercicio agregado

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -92,11 +92,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  var promedio=0;
-  for (var i=0; i<resultadosTest.length; i++){
-    promedio=(promedio+resultadosTest[i])/i;
-  }
-  return promedio;
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
