53f04286e3464ed9ff8163998ee0bf4bc62159d2
TheMacrom <flores.gaston2@gmail.com>
2022-09-14 15:12:51 -0300

    aksja

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -100,7 +100,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  return numeros.Math.max();
+  var grande = numeros[0];
+  for (var i=0; i<numeros.length; i++){
+    if (numeros[i]> grande){
+      grande = numeros[i];
+    }
+  }
+  return numeros grande;
 }
 
 
