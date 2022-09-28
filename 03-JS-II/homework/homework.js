0f27a81a5318b4d6dcdb02e67fc760f53d3ca406
TheMacrom <flores.gaston2@gmail.com>
2022-09-28 11:49:56 -0300

    Ejercicios resueltos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -156,17 +156,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(numero<2){
-    return false;
-  }
-  for (i=1; i<=20; i++){
+  if(numero<2) return false;
+  if(numero === 2 ) return true;
+  for (var i=2; i<numero; i++){
     if(numero%i===0){
-      return true;
-    }else{
       return false;
     }
   }
-}
+      return true;
+  }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -184,7 +182,7 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí  
   var arraytablaDel6=[] 
-  for (i===0; i<11; i++){
+  for (var i=0; i<11; i++){
     arraytablaDel6.push(6*i);
   }
   return arraytablaDel6;
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -181,13 +181,31 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
-
+  var nuevoArray= [];
+  for(let i=0; i<array.length; i++){
+    if(array[i]==="Enero" || array[i]==="Marzo" || array[i]==="Noviembre"){
+      nuevoArray.push(array[i]);
+      }
+    }
+    if(nuevoArray.length<3){
+      return "No se encontraron los meses pedidos";
+    }
+    else{
+      return nuevoArray;
+    }
+  }
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray = [];
+  for(let i=0; i<array.length; i++){
+    if(array[i]>100){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 
@@ -199,6 +217,17 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array = [];
+  var i = 0;
+    while(i<10){
+      numero=numero+2;
+      array.push(numero);
+      i=i+1;
+      if(numero===i){
+        return "Se interrumpió la ejecución";
+        break;
+      } 
+    }return array;
 }
 
 
@@ -209,6 +238,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  for(let i=0; i<10; i++){
+    if(i===5)
+    continue;
+    else{
+    numero=numero+2;
+    array.push(numero);
+  } 
+}
+return array;
 }
 
 
